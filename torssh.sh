0970ed915f391c19bb66fc133cf34dddbd2d8ce9
Marek Motycka <motycka.marek@gmail.com>
2018-05-06 01:02:19 +0200

    ssh opts enabled

--- torssh.sh
+++ torssh.sh
@@ -3,10 +3,19 @@
 #$1 ... 'user@host'
 
 if [ "$#" -lt 1 ] ; then
-  echo "Usage: $0 user@host commands"
+  echo "Usage: $0 options user@host commands"
 fi
 
 temp_fp=$(mktemp)
+
+arg_act="$1"
+ssh_opts=""
+
+while echo "${arg_act}" | grep -v '@' ; do
+  ssh_opts+="${arg_act} "
+  shift
+  arg_act="$1"
+done
 
 user=$(echo "$1" | cut -d'@' -f1)
 host=$(echo "$1" | cut -d'@' -f2 | cut -d' ' -f1)
@@ -21,6 +30,6 @@ Host torhost
   ProxyCommand connect -4 -S localhost:9050 $(tor-resolve %h localhost:9050) %p
 ' > ${temp_fp}
 
-eval "ssh -F ${temp_fp} ${user}@torhost $@"
+eval "ssh -F ${temp_fp} ${ssh_opts} ${user}@torhost $@"
 
 rm "${temp_fp}"
