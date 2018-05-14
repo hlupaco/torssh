54546be5ecb7b1233648eb14ea78efebe4f760e1
Marek Motycka <motycka.marek@gmail.com>
2018-05-15 00:46:06 +0200

    options, prepare only

--- torssh.sh
+++ torssh.sh
@@ -2,8 +2,23 @@
 
 #$1 ... 'user@host'
 
+prepare_only=""
+while getopts ':p' opt ; do
+  case "${opt}" in
+    p)
+      prepare_only="y"
+      ;;
+    \?)
+      echo "Wrong option: ${opt}"
+      exit 1
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
 if [ "$#" -lt 1 ] ; then
-  echo "Usage: $0 options user@host commands"
+  echo "Usage: $0 -p options user@host commands"
 fi
 
 temp_fp=$(mktemp)
@@ -29,6 +44,11 @@ Host torhost
   Protocol 2
   ProxyCommand connect -4 -S localhost:9050 $(tor-resolve %h localhost:9050) %p
 ' > ${temp_fp}
+
+if [ "${prepare_only}" = "y" ] ; then
+  echo "${temp_fp}"
+  exit 0
+fi
 
 eval "ssh -F ${temp_fp} ${ssh_opts} ${user}@torhost $@"
 
